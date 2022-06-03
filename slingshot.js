2a003851992a3ab1b171c8cdfeecff25262c0f6f
knjh12 <90729381+knjh12@users.noreply.github.com>
2022-06-03 17:51:49 -0300

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,27 @@
+class slingShot{
+    constructor(bodyA,pointB){
+       var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:1
+        }
+ 
+        this.sling=Constraint.create(options);
+        World.add(world,this.sling);
+        this.pointB=pointB;
+    }
+    
+    fly(){
+        this.sling.bodyA=null;
+
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var posA=this.sling.bodyA.position;
+            line(posA.x,posA.y, this.pointB.x,this.pointB.y);
+        }
+    }
+
+}
